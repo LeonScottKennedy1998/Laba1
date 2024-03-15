d2e4e083dc9e91ff9b93bf9d0f15ef82bb442c0a
LeonScottKennedy1998 <karina_alekseeva_2006@mail.ru>
2024-03-15 18:42:24 +0300

    Лабораторная работа №1

---
+++ Laba1/Window3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Laba1
+{
+   
+    public partial class Window3 : Window
+    {
+        private DogPriutEntities dogpriut = new DogPriutEntities();
+        public Window3()
+        {
+            InitializeComponent();
+            Grid3.ItemsSource = dogpriut.Dogs.ToList();
+        }
+    }
+}
